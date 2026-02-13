8098c2a738c538e2110c94c2781cb16f321389f0
Chouchelou <168338673+Chouchelou@users.noreply.github.com>
2026-02-13 08:16:57 +0100

    Refactor main.py to use MVC pattern
    Changement du main afin d'apporter un modèle MVC au code et structure de donnée plus aéré

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import sys
 from PyQt6.QtWidgets import QApplication
-from PyQt6.QtGui import QFont
-from morpion import MorpionPyQt
+from Model.morpion_model import MorpionModel
+from View.morpion_view import MorpionView
+from Controller.morpion_controller import MorpionController
 
 def main():
     app = QApplication(sys.argv)
-    app.setFont(QFont("Arial", 12))
-    jeu = MorpionPyQt()
-    jeu.showMaximized()
+    
+    # Instanciation MVC
+    model = MorpionModel()
+    view = MorpionView()
+    
+    # On donne tout au contrôleur
+    controller = MorpionController(model, view)
+    
+    # On connecte la vue au contrôleur (pour le closeEvent)
+    view.set_controller(controller)
+
+    view.show()
     sys.exit(app.exec())
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     main()
